480610259831f80383685d71f0375d38da34cea4
RichDillard24 <richiegd@hotmail.com>
2026-01-10 13:03:29 -0800

    Initial Commit

---
+++ BatteryManager Watch App/BatteryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryManagerApp.swift
+//  BatteryManager Watch App
+//
+//  Created by Richard Dillard on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryManager_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
